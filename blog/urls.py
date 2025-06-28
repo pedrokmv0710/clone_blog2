190fbae4d6df97930e0224154bc476b6d823f304
pedrokmv0710 <keikoalberto36@gmail.com>
2025-06-28 17:43:42 -0400

    post update

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/create/', PostCreateView.as_view(), name='post-create'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post-update'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 # Create your views here.
 
@@ -19,3 +19,9 @@ class PostCreateView(CreateView):
     model = Post 
     template_name = 'post-create.html'
     fields = ['title', 'content', 'author']
+    
+# update view
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title', 'content']
